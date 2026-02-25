949c6526aa56a45f95cfd62f2c78de26b2e8a9e5
Phil Howard <github@gadgetoid.com>
2026-02-25 15:07:39 +0000

    Don't leak shapes.

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@ int main() {
     brush_t* black = new color_brush_t(rgb_color_t(0, 0, 0, 255));
     brush_t* darkred = new color_brush_t(rgb_color_t(128, 0, 0, 255));
     brush_t* green = new color_brush_t(rgb_color_t(0, 255, 0, 255));
+    shape_t* shape = squircle(0, 0, 30, 3.0f);
 
     unsigned a = 0;
     while (true) {
@@ -36,12 +37,10 @@ int main() {
         screen->brush(black);
         screen->rectangle(rect_t(10, 10, 50, 50));
         screen->brush(green);
-        shape_t* shape = squircle(0, 0, 30, 3.0f);
         mat3_t transform = mat3_t();
         transform.translate(80, 60);
         transform.rotate(a);
         shape->transform = transform;
-
         screen->shape(shape);
         display.update();
         a += 1;
