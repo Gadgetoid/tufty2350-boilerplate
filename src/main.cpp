be16b7ece79cadd912d819060987ba857fc43c0b
Phil Howard <github@gadgetoid.com>
2026-02-25 12:20:17 +0000

    Draw an antialiased squircle.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "powman.h"
 #include "picovector.hpp"
+#include "primitive.hpp"
 #include "color.hpp"
 #include "brush.hpp"
 #include "st7789.hpp"
@@ -19,6 +20,7 @@ int main() {
     display.set_backlight(255);
     display.set_mode(false);
     screen = new image_t((void *)display.get_framebuffer(), 160, 120);
+    screen->antialias(X4);
 
     uint16_t v = 0;
     while (true) {
@@ -28,6 +30,10 @@ int main() {
         brush = new color_brush_t(rgb_color_t(0, 0, 0, 255));
         screen->brush(brush);
         screen->rectangle(rect_t(10, 10, 50, 50));
+        brush = new color_brush_t(rgb_color_t(0, 255, 0, 255));
+        screen->brush(brush);
+        shape_t* shape = squircle(80, 60, 30, 3.0f);
+        screen->shape(shape);
         display.update();
     }
 }
